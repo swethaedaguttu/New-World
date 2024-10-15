acef7f493feb8c6ee40601da7d92cc64a81b16a8
swethaedaguttu <140315099+swethaedaguttu@users.noreply.github.com>
2024-10-15 13:04:21 +0530

    Update wsgi.py

--- community_connect/wsgi.py
+++ community_connect/wsgi.py
@@ -8,11 +8,8 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'community_connect.settings')
 
 application = get_wsgi_application()
-
-gunicorn community_connect.wsgi:application --bind 0.0.0.0:8000 --workers 3
